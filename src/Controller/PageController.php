da213257ec533a5b5653c36a0016c4484947361f
Marc Neuhaus <marc@neuhaus.nrw>
2026-06-12 11:03:42 +0200

    Serve root app icons for crawlers

--- src/Controller/PageController.php
+++ src/Controller/PageController.php
@@ -6,8 +6,10 @@ namespace App\Controller;
 
 use App\Repository\SourceRepository;
 use App\Service\RegionContext;
-use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\DependencyInjection\Attribute\Autowire;
+use Symfony\Component\HttpFoundation\BinaryFileResponse;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -17,7 +19,11 @@ use Symfony\Component\Routing\Attribute\Route;
  */
 final class PageController extends AbstractController
 {
-    public function __construct(private readonly RegionContext $regions)
+    public function __construct(
+        private readonly RegionContext $regions,
+        #[Autowire('%kernel.project_dir%')]
+        private readonly string $projectDir,
+    )
     {
     }
 
@@ -54,6 +60,7 @@ final class PageController extends AbstractController
     }
 
     #[Route('/manifest.webmanifest', name: 'site_manifest', methods: ['GET'])]
+    #[Route('/site.webmanifest', name: 'site_manifest_compat', methods: ['GET'])]
     public function manifest(): JsonResponse
     {
         $region = $this->regions->current();
@@ -72,7 +79,20 @@ final class PageController extends AbstractController
                 ['src' => '/icons/icon-192.png', 'sizes' => '192x192', 'type' => 'image/png', 'purpose' => 'any maskable'],
                 ['src' => '/icons/icon-512.png', 'sizes' => '512x512', 'type' => 'image/png', 'purpose' => 'any maskable'],
             ],
-        ]);
+        ], Response::HTTP_OK, ['Content-Type' => 'application/manifest+json']);
+    }
+
+    #[Route('/favicon.ico', name: 'favicon_ico', methods: ['GET', 'HEAD'])]
+    public function favicon(): BinaryFileResponse
+    {
+        return $this->iconResponse('icon-192.png');
+    }
+
+    #[Route('/apple-touch-icon.png', name: 'apple_touch_icon', methods: ['GET', 'HEAD'])]
+    #[Route('/apple-touch-icon-precomposed.png', name: 'apple_touch_icon_precomposed', methods: ['GET', 'HEAD'])]
+    public function appleTouchIcon(): BinaryFileResponse
+    {
+        return $this->iconResponse('apple-touch-icon.png');
     }
 
     #[Route('/robots.txt', name: 'robots_txt', methods: ['GET'])]
@@ -91,5 +111,18 @@ final class PageController extends AbstractController
             TXT;
 
         return new Response($body, Response::HTTP_OK, ['Content-Type' => 'text/plain; charset=utf-8']);
+    }
+
+    private function iconResponse(string $filename): BinaryFileResponse
+    {
+        $response = new BinaryFileResponse($this->projectDir.'/public/icons/'.$filename);
+        $response->headers->set('Content-Type', 'image/png');
+        $response->headers->set('X-Robots-Tag', 'noindex');
+        $response->setPublic();
+        $response->setMaxAge(31536000);
+        $response->setSharedMaxAge(31536000);
+        $response->setImmutable();
+
+        return $response;
     }
 }
